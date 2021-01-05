5f85615bb589eecf7545de852fe828647b81ba06
viganzeqiri <viganzeqirii@gmail.com>
2021-01-05 20:05:01 +0100

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+function App() {
+  return (
+    <div>
+      <h1>Hi there</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+function mount(element) {
+  ReactDOM.render(<App />, element);
+}
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
